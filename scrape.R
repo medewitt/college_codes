32a06d775bfd937251a4a824dca2795c81f82868
michael vbox <me.dewitt.jr@gmail.com>
2018-04-23 11:33:49 -0400

    updated

--- scrape.R
+++ scrape.R
@@ -6,6 +6,7 @@
 library(dplyr)
 library(tabulizer)
 library(stringr)
+library(countrycode)
 
 
 # download data -----------------------------------------------------------
@@ -19,21 +20,44 @@ sat_scores <- "sat_2017.pdf"
 
 
 
+# Define Page and Column Size ---------------------------------------------
+
+
 columns <- list(col_one = c(1,1, 790, 137), col_2 = c(1,135, 790, 250), col_3 = c(1,255, 770, 360), 
                 col_4 =c(1,355, 770, 475),
                 last_one = c(1,475, 770, 600))
 
+
+# Country and State Name Filters ------------------------------------------
+
+countries <- countrycode::codelist$country.name.en
+
+state_names <- state.name
+
+names_to_remove <- c(countries, state_names)
+
+cleaner <- paste0("\\n(", names_to_remove, ")\\n")
+
 big_list <- c()
 for(i in 2:22){
+  i <- 2
   tab <- extract_tables(sat_scores, page = c(i,i,i,i,i), area = columns, guess = FALSE, method = "character")
   
   collector <- c()
   for(j in 1:5){
-    to_inspect <-gsub(tab[[j]], pattern = "\\n|\\t", replacement = " ")
+    #to_inspect <-gsub(tab[[j]], pattern = "\\n(Alaska)\\n", replacement = "")
+    j <-2
+    
+    to_inspect <- tab[[j]]
+    for(k in seq_along(cleaner)){
+      to_inspect <-gsub(to_inspect, pattern = cleaner[[k]], replacement = "")
+    }
+    #to_inspect <- purrr::map(tab[[j]], .f = str_replace_all, pattern = cleaner, replacement = " ")[[1]]
+    to_inspect <-gsub(to_inspect, pattern = "\\n|\\t", replacement = " ")
     to_inspect_b <-gsub(to_inspect, pattern = "^[A-z]+", replacement = " ")
     to_inspect_c <-gsub(to_inspect_b, pattern = "^\\s+", replacement = "")
     message(paste0("Sheet", j ,"of", i, sep = " "))
-    numbers <- t(str_extract_all(pattern = "[0-9]{4}", string = to_inspect_b, simplify = TRUE))
+    numbers <- str_extract_all(pattern = "[0-9]{4}", string = to_inspect_b, simplify = TRUE)[1,]
     #school <-t(str_extract_all(pattern = "[^0-9]+", string = to_inspect_c, simplify = TRUE))
     school <-str_trim(str_split(to_inspect_c, pattern = "[0-9]{4}")[[1]][-1])
     output_df <- cbind(numbers = numbers, school = school)%>% as_data_frame()
